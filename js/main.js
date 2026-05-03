797de279d525dfc719e2ac3b14cb02fd8271c3b6
tumyr546 <gulakovtimur03@gmail.com>
2026-05-03 18:26:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const arr = [
+  "apple", "banana", "orange", "grape", "kiwi", "mango",
+  "pear", "peach", "plum", "cherry", "melon", "lemon",
+  "lime", "apricot", "fig", "date", "coconut", "papaya",
+  "pineapple", "blueberry", "strawberry", "raspberry", "blackberry", "cranberry",
+  "watermelon", "pomegranate", "guava", "lychee", "passionfruit", "dragonfruit",
+  "tangerine", "nectarine", "grapefruit", "persimmon", "quince", "currant",
+  "mulberry", "elderberry", "gooseberry", "starfruit", "jackfruit", "durian",
+  "avocado", "olive", "tomato", "cucumber", "pumpkin", "zucchini"
+];
+
+
+console.log(arr[arr.length])
+// arr[arr.length] = 100
+// arr[arr.length] = 150
+// arr[arr.length] = 200
+console.log(arr)
+
+// console.log(arr[0])
+// console.log(arr[1])
+
+// let u = 0
+
+
+
+// for(let i = 0; i < arr.length;i++){
+//     u += arr[i]
+// }
+
+// console.log(u)
